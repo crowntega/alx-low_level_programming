c7f3da5051cd1102ca469889ed5964a1fe23537a
Stephanie <tegaomolu@gmail.com>
2023-02-16 03:44:06 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that executed a line with puts functions
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
